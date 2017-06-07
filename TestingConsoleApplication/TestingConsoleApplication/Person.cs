442ebe59944cada4610c98eb42504873425a15d2
fmlpt <filipe.manuel@gmail.com>
2017-06-07 12:51:07 +0100

    Added files

---
+++ TestingConsoleApplication/TestingConsoleApplication/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestingConsoleApplication
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ TestingConsoleApplication/TestingConsoleApplication/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ParseDate();
+
+            // TestGrouping();
+
+            ParseDate2();
+        }
+
+        private static void ParseDate2()
+        {
+            DateTime selectedDate;
+            if(DateTime.TryParseExact("551105", "yyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.None, out selectedDate))
+            {
+                Console.WriteLine($"Yay! Date: {selectedDate}");
+                return;
+            }
+
+            Console.WriteLine("Booo!");
+        }
+
+        private static void TestGrouping()
+        {
+            var people = new List<Person>
+            {
+                new Person { Name="John", Gender="M", BirthDate = new DateTime(1965, 5, 8) },
+                new Person { Name="Peter", Gender="M", BirthDate = new DateTime(1995, 12, 6) },
+                new Person { Name="Kate", Gender="F", BirthDate = new DateTime(2000, 4, 3) }
+            };
+
+
+            var res = people.GroupBy(p => p.Gender).Select(g => new Tuple<string, List<Person>>(g.Key, g.ToList())).ToList();
+
+            res.ForEach(r => {
+                Console.WriteLine();
+                Console.WriteLine("Gender: " + r.Item1);
+
+                r.Item2.ForEach(rg =>
+                {
+                    Console.WriteLine($"Name: {rg.Name} Birth date: {rg.BirthDate:dd/MM/yyyy}");
+                });
+            });
+        }
+
+        private static void ParseDate()
+        {
+            var res = DateTime.ParseExact("20170519 100322", "yyyyMMdd hhmmss", CultureInfo.InvariantCulture);
+        }
+    }
+}
